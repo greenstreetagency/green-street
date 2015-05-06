0f341dd8c45b6aebfcaed93f63a96741dd2c8dd0
Stefan Bowerman <stefbowerman@gmail.com>
2015-05-05 17:00:54 -0700

    Code cleanup

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -124,225 +124,212 @@ var ScrollUpBtn = require('./components/ScrollUpBtn.js');
    * @returns {Number}
    */
   function getOffsetForScene(sceneKey) {
-    var headerHeight = getHeaderHeight();
-    return -headerHeight; //(e == "home") ? 0 : -headerHeight; ???
+    // var headerHeight = getHeaderHeight();
+    return sceneKey == "home" ? 0 : -getHeaderHeight();  //???
+    // return -getHeaderHeight();
   }
 
   /**
    * Adjusts slide height to ensure that they take up the full viewport height minus the header height
    */
   function adjustSlideDimensions() {
+    // $slides.height( window.innerHeight - getHeaderHeight() );
     $slides.height( window.innerHeight - getHeaderHeight() );
   }
 
-  function addSceneAnimations() {
-    // Home Section
-    var blurAmount = { a : 3 };
-    var tL = new TimelineMax();
+  // Dictionary for different scene animation styles (home section, about section, process section etc..)
+  // used in addSceneAnimationForType;
+  var sceneAnimations = {
+    home : function( scene ) {
+      var blurAmount  = { a : 3 }; // Starts blur at 3px
+      var tL          = new TimelineMax();
+      var tweenGroup  = [];
 
-    var applyBlur = function() {
-      TweenMax.set('.home-bg', {
-        webkitFilter : "blur(" + blurAmount.a + "px)"
-      })
-    }
-
-    TweenMax.set('#home-tagline-2', {
-      alpha: 0,
-      y: '0%',
-      scale: 0.9,
-    });
-
-    var firstStageTweens = [];
-        firstStageTweens.push(TweenMax.to('#home-tagline-1', 1, {
-          alpha: 0,
-          y: '-10%'
-        }));
-
-    if(!touchEnabled){
-      // Only apply blur tween to non touch browsers, it's intensive
-      applyBlur();
-      firstStageTweens.push(TweenMax.to(blurAmount, 1, {
-        a : 0,
-        onUpdate : applyBlur
-      }))
-    }
-
-    tL.add(firstStageTweens)
-    .add(TweenMax.to('#home-tagline-2', 1, {
-      alpha: 1,
-      scale: 1
-    }, "-=0.5"));
-
-    scenes.home.setTween(tL);
-
-    // Branding Section
-
-    var $brandSlide = $('#branding.slide');
-    var $brandSlideSteps = $brandSlide.find('.about-step');
-    var tL = new TimelineMax();
-
-    var $first = $brandSlideSteps.first();
-    var $notFirst = $brandSlideSteps.not($first);
-
-    tL.add(TweenMax.to($first.get(0), 1, {
-      alpha: 0
-    }));
-
-    $notFirst.css({'opacity' : 0})
-    $notFirst.each(function(i, el){
-      // console.log((i != $notFirst.size() - 1));
-      var yoyo = (i != $notFirst.size() - 1);
-      var repeat = yoyo ? 1 : 0
-      tL.add(TweenMax.to(el, 1, {
-        alpha: 1,
-        repeat: repeat,
-        yoyo: yoyo
-      }))
-    });
-
-    tL.add(TweenMax.to($brandSlide.get(0), 1, {
-      backgroundColor : '#55BDE8'
-    }), "-=0.5");
-
-    scenes.branding.setTween(tL);
-
-    // Touch points section
-
-    var $touchpointsSlide = $('#touch-points.slide');
-    var $touchpointsSlideSteps = $touchpointsSlide.find('.about-step');
-    var tL = new TimelineMax();
-
-    var $first = $touchpointsSlideSteps.first();
-    var $notFirst = $touchpointsSlideSteps.not($first);
-
-    tL.add(TweenMax.to($first.get(0), 1, {
-      alpha: 0
-    }));
-
-    $notFirst.css({'opacity' : 0})
-    $notFirst.each(function(i, el){
-      // console.log((i != $notFirst.size() - 1));
-      var yoyo = (i != $notFirst.size() - 1);
-      var repeat = yoyo ? 1 : 0
-      tL.add(TweenMax.to(el, 1, {
-        alpha: 1,
-        repeat: repeat,
-        yoyo: yoyo
-      }))
-    });
-
-    tL.add(TweenMax.to($('#touch-points.slide').get(0), 1, {
-      backgroundColor : '#CA277E'
-    }));
-
-    scenes.touchPoints.setTween(tL);
-
-    // Executions Section
-
-    var $executionsSlide = $('#executions.slide');
-    var $executionsSlideSteps = $executionsSlide.find('.about-step');
-    var tL = new TimelineMax();
-
-    var $first = $executionsSlideSteps.first();
-    var $notFirst = $executionsSlideSteps.not($first);
-
-    tL.add(TweenMax.to($first.get(0), 1, {
-      alpha: 0
-    }));
-
-    $notFirst.css({'opacity' : 0})
-    $notFirst.each(function(i, el){
-      // console.log((i != $notFirst.size() - 1));
-      var yoyo = (i != $notFirst.size() - 1);
-      var repeat = yoyo ? 1 : 0
-      tL.add(TweenMax.to(el, 1, {
-        alpha: 1,
-        repeat: repeat,
-        yoyo: yoyo
-      }))
-    });
-
-    tL.add(TweenMax.to($executionsSlide.get(0), 1, {
-      backgroundColor : '#222'
-    }));
-
-    scenes.executions.setTween(tL);
-
-    // Process Section
-
-    var tL = new TimelineMax();
-
-    var $processStepsInline  = $('.process-step-inline');
-    var $processStepsAbs     = $('.process-step-absolute');
-    var $processDescriptions = $('.process-step-description');
-    var processStepCount     = $processStepsInline.size();
-
-    // tL.add(TweenMax.to($processStepsInline[i], 1, {
-    //   alpha: 0
-    // }));
-
-    $processStepsInline.css({'opacity' : 0.1});
-    $processStepsAbs.css({'opacity' : 0});
-    $processDescriptions.css({'opacity' : 0})
-
-    for (var i = 0; i < processStepCount; i++) {
-      var stepInline = $processStepsInline[i];
-      var stepAbs = $processStepsAbs[i];
-      var stepAbsImg = $(stepAbs).find('img').first();
-      var stepDescription = $processDescriptions[i];
-      var lastStep = (i == processStepCount - 1);
-      var yoyo = (i != processStepCount - 1);
-      var repeat = yoyo ? 1 : 0;
-
-      var tweens = [];
-
-      var stageOneTweens = [];
-      var stageTwoTweens = [];
-
-      stageOneTweens.push(TweenMax.to(stepInline, 100, {
-        alpha : 1,
-      }));
-
-      stageOneTweens.push(TweenMax.to(stepDescription, 100, {
-        alpha : 1,
-      }));
-
-      stageOneTweens.push(TweenMax.to(stepAbs, 100, {
-        alpha : 1,
-      }));
-
-      stageOneTweens.push(TweenMax.fromTo(stepAbsImg, 80, {
-        x: "150%",
-        alpha: 0,
-        force3D: true
-      }, {
-        x: "0%",
-        alpha: 1,
-        force3D: true
-      }));
-
-      tL.add(stageOneTweens);
-
-      if(!lastStep){
-
-        stageTwoTweens.push(TweenMax.to(stepDescription, 100, {
-          alpha : 0,
-        }));
-
-        stageTwoTweens.push(TweenMax.to(stepAbs, 100, {
-          alpha : 0,
-        }));
-
-        stageTwoTweens.push(TweenMax.to(stepAbsImg, 80, {
-          x: "-150%",
-          alpha: 0
-        }));
-
+      var applyBlur = function() {
+        TweenMax.set('.home-bg', {
+          webkitFilter : "blur(" + blurAmount.a + "px)"
+        })
       }
 
-      tL.add(stageTwoTweens, "+=50");
-    };
-    scenes.process.setTween(tL);
+      TweenMax.set('#home-tagline-2', {
+        alpha: 0,
+        y: '0%',
+        scale: 0.9,
+      });
+
+      tweenGroup.push(
+        TweenMax.to('#home-tagline-1', 1, {
+          alpha: 0,
+          y: '-10%'
+        })
+      );
+
+      if(!touchEnabled){ // Only apply blur tween to non touch browsers, it's intensive
+        applyBlur();
+        tweenGroup.push(
+          TweenMax.to(blurAmount, 1, {
+            a : 0,
+            onUpdate : applyBlur
+          })
+        );
+      }
+
+      tL
+      .add(tweenGroup)
+      .add(TweenMax.to('#home-tagline-2', 1, {
+        alpha: 1,
+        scale: 1
+      }, "-=0.5"));
+
+      scene.setTween(tL);
+
+    },
+    process : function( scene ) {
+      var tL = new TimelineMax();
+
+      var $processStepsInline  = $('.process-step-inline');
+      var $processStepsAbs     = $('.process-step-absolute');
+      var $processDescriptions = $('.process-step-description');
+      var processStepCount     = $processStepsInline.size();
+
+      $processStepsInline.css({'opacity' : 0.1});
+      $processStepsAbs.css({'opacity' : 0});
+      $processDescriptions.css({'opacity' : 0})
+
+      for (var i = 0; i < processStepCount; i++) {
+        var stepInline = $processStepsInline[i];
+        var stepAbs = $processStepsAbs[i];
+        var stepAbsImg = $(stepAbs).find('img').first();
+        var stepDescription = $processDescriptions[i];
+        var lastStep = (i == processStepCount - 1);
+
+        var tweenGroups = {
+          first  : [],
+          second : []
+        }
+
+        tweenGroups.first.push(
+          TweenMax.to(stepInline, 100, {
+            alpha : 1,
+          })
+        );
+
+        tweenGroups.first.push(
+          TweenMax.to(stepDescription, 100, {
+            alpha : 1,
+          })
+        );
+
+        tweenGroups.first.push(
+          TweenMax.to(stepAbs, 100, {
+            alpha : 1,
+          })
+        );
+
+        tweenGroups.first.push(
+          TweenMax.fromTo(stepAbsImg, 80,
+            {
+              x: "150%",
+              alpha: 0,
+              force3D: true
+            },
+            {
+              x: "0%",
+              alpha: 1,
+              force3D: true
+            }
+          )
+        );
+
+        if(!lastStep){
+
+          tweenGroups.second.push(
+            TweenMax.to(stepDescription, 100, {
+              alpha : 0,
+            })
+          );
+
+          tweenGroups.second.push(
+            TweenMax.to(stepAbs, 100, {
+              alpha : 0,
+            })
+          );
+
+          tweenGroups.second.push(
+            TweenMax.to(stepAbsImg, 80, {
+              x: "-150%",
+              alpha: 0
+            })
+          );
+
+        }
+
+        tL
+        .add(tweenGroups.first)
+        .add(tweenGroups.second, "+=50");
+      };
+
+      scene.setTween(tL);
+
+    },
+    about : function( scene, opts ) {
+      var bgColor = opts.bgColor;
+      var $slide  = opts.$slide
+      var $slides = $slide.find('.about-step');
+      var tL      = new TimelineMax();
+
+      var $first = $slides.first();
+      var $remaining = $slides.slice(1);
+      var remainingCount = $remaining.size();
+
+      tL.add(TweenMax.to($first.get(0), 1, {
+        alpha: 0
+      }));
+
+      $remaining
+      .css({'opacity' : 0})
+      .each(function(i, el){
+
+        var yoyo = (i + 1 != remainingCount);
+
+        tL.add(TweenMax.to(el, 1, {
+          alpha: 1,
+          yoyo: yoyo,
+          repeat: (yoyo ? 1 : 0)
+        }));
+
+      });
+
+      tL.add(TweenMax.to($slide.get(0), 1, {
+        backgroundColor : bgColor
+      }), "-=0.5");
+
+      scene.setTween(tL);
+    }
+  };
+
+  function addSceneAnimationForType(key, scene, opts) {
+    if(sceneAnimations.hasOwnProperty(key))
+      sceneAnimations[key].call( this, scene, opts );
+  }
+
+  function addSceneAnimations() {
+
+    // Add animations for the home scene
+    addSceneAnimationForType( 'home', scenes.home );
+
+    // Branding Section
+    addSceneAnimationForType( 'about', scenes.branding, { bgColor : '#55BDE8', $slide : $('#branding.slide')  } );
+
+    // Touch points section
+    addSceneAnimationForType( 'about', scenes.touchPoints, { bgColor : '#CA277E', $slide : $('#touch-points.slide')  } );
+
+    // Executions Section
+    addSceneAnimationForType( 'about', scenes.executions, { bgColor : '#222', $slide : $('#executions.slide')  } );
+
+    // Add animations for the process scene
+    addSceneAnimationForType( 'process', scenes.process );
+
   }
 
   function initScrollControls() {
@@ -378,13 +365,7 @@ var ScrollUpBtn = require('./components/ScrollUpBtn.js');
     scenes.executions = new ScrollMagic.Scene({
       triggerElement: '#executions'
     })
-    .setPin("#executions")
-    // .on("end", function() {
-    //     $("#executions").addClass("slide--fixed")
-    // })
-    // .on("start", function() {
-    //     $("#executions").removeClass("slide--fixed")
-    // });
+    .setPin("#executions");
 
     scenes.process = new ScrollMagic.Scene({
       triggerElement: '#process'
@@ -393,12 +374,11 @@ var ScrollUpBtn = require('./components/ScrollUpBtn.js');
 
     scenes.clients = new ScrollMagic.Scene({
       triggerElement: '#clients'
-    })
-    // .setPin("#clients");
+    });
 
     scenes.footer = new ScrollMagic.Scene({
       triggerElement: 'footer'
-    })
+    });
 
     scrollController.addScene([
       scenes.home,
@@ -409,7 +389,7 @@ var ScrollUpBtn = require('./components/ScrollUpBtn.js');
       scenes.clients,
       scenes.footer
     ]);
-    // window.s = scrollController;
+
     addSceneAnimations();
   }
 
